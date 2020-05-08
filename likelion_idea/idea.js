e11514ebc9d0db5916655d444c16bd5bc398afaf
aeuna <aheunlee0113@gmail.com>
2020-05-08 20:38:25 +0900

    Add idea

--- likelion_idea/idea.js
+++ likelion_idea/idea.js
@@ -1,5 +1,4 @@
 let all = document.querySelectorAll("#img_btn")
-console.log(all.length)
 all[0].addEventListener('click',function(){
     window.scrollBy({left:0, top:1080, behavior:'smooth'});
 })
@@ -30,6 +29,17 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 2000); 
 }
+ 
+var modal1 = document.getElementById('myModal1');
+var btn1 = document.getElementById("modal_btn_menu");
+var span1 = document.getElementsByClassName("close")[1];  
+                                      
+btn1.onclick = function() {
+    modal1.style.display = "block"; 
+}
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
 
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("modal_btn");
@@ -37,6 +47,7 @@ var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+    flag = 0;
 }
 span.onclick = function() {
     modal.style.display = "none";
@@ -45,19 +56,6 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
-
-var modal1 = document.getElementById('myModal1');
-var btn1 = document.getElementById("modal_btn_menu");
-var span1 = document.getElementsByClassName("close")[1];                                          
-
-btn1.onclick = function() {
-    modal1.style.display = "block";
-}
-span1.onclick = function() {
-    modal1.style.display = "none";
-}
-window.onclick = function(event) {
     if (event.target == modal1) {
         modal1.style.display = "none";
     }
@@ -71,3 +69,37 @@ setInterval(function(){
     var s = timer.getSeconds();
     document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
 },1000);
+
+
+var effect_box = document.getElementsByClassName('text-effect');
+
+function effect(object, speed) {
+
+    var object_len = object.item(0).innerText.length;
+    var object_text = object.item(0).innerText;
+
+    object.item(0).innerHTML = '';
+
+    for (var i = 0; i <= object_len; i++) {
+        n_tag = document.createElement("p");
+        object.item(0).append(n_tag);
+        n_tag.append(object_text.charAt(i))
+
+        if (i >= object_len) {
+            var anima = true;
+        }
+    }
+    if (anima === true) {
+        var turn = 0;
+        var opacity_txt = setInterval(function () {
+            document.querySelectorAll('p').item(turn).style.opacity = '1';
+            turn++;
+
+            if (object_len === turn) {
+                clearInterval(opacity_txt);
+            }
+        }, speed);
+    }
+}
+
+effect(effect_box, 100);
